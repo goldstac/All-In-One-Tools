9c39e8a2c3eb43442b62ddf25a5ee1c010c35932
Li Productions <172457372+goldstac@users.noreply.github.com>
2025-11-03 20:57:32 +0400

    Update main.c

--- main.c
+++ main.c
@@ -6,8 +6,14 @@ int main() {
     char main_input[256];
     printf("Welcome To All In One Tools\n");
     printf("Type help To See All The Commands\n");
+    while (1) {
     fgets(main_input,sizeof(main_input),stdin);
-    if (strcmp(main_input,"help")) {
+    main_input[strcspn(main_input,"\n")] = '\0';
+    if (strcmp(main_input,"help") == 0) {
         printf("1 : calculate\n");
+    }
+    if (strcmp(main_input,"calculate") == 0) {
+        calculator();
+    }
     }
 }
