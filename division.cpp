7408eeddfbd9d573be818723e9f337340d6bc386
Li Productions <172457372+goldstac@users.noreply.github.com>
2025-11-04 14:37:28 +0400

    Create division.cpp

---
+++ division.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "division-cpp.h"
+extern "C" void division() {
+    int first_number;
+    int second_number;
+    std::cout << "Enter First Number\n";
+    std::cin >> first_number;
+    std::cout << "Enter Second Number\n";
+    std::cin >> second_number;
+    int calculate;
+    calculate = first_number / second_number;
+    std::cout << "Total Is" << " " << calculate;
+}
